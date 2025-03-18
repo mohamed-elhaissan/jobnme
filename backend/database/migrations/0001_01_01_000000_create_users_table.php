a2b1333d29d34092cb97b643bdea2c5d3ef6e27f
Mohamedtwd <superraja2003@gmail.com>
2025-03-18 13:24:45 +0000

    modifier UserSeeder

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration {
             $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
+            $table->string('nationality');
+            $table->enum('sexe',['male', 'female']);
+            $table->date('date_of_birth');
             $table->string('password');
             $table->enum('role', ['job_seeker', 'employer', 'admin'])->default('job_seeker');
             $table->rememberToken();
